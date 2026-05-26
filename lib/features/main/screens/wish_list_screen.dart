b44cc171f489225a765990141b92afda6550f706
kkdjanakaeranda <erandak-ct21027@stu.kln.ac.lk>
2026-05-26 09:23:49 +0530

    chnage wish list to scaffold

--- lib/features/main/screens/wish_list_screen.dart
+++ lib/features/main/screens/wish_list_screen.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class WishListScreen extends StatelessWidget {
   const WishListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Wish List Screen'),
+    return Scaffold(
+      body: Center(
+        child: Text('Wish List Screen'),
+      ),
     );
   }
 }
